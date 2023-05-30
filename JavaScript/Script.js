c43d874e5e4077759fe3395054181c953fa1587e
Cássio Souza <cassiosouza248@gmail.com>
2023-05-30 15:44:16 -0300

    Index v.1

---
+++ JavaScript/Script.js
@@ -0,0 +1,6 @@
+document.getElementById('animation').addEventListener('mouseover', animation);
+
+function animation(){
+    document.getElementById('animation').style.visibility = 'visible';
+    document.getElementById('animation').style.opacity = 1;
+}
